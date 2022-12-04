05ec7e78de23ff037b0be8aa279532aff9355549
Leanne Sto. Domingo <leanneasd@gmail.com>
2022-12-04 06:42:53 +0100

    Improve day 4

--- 2022/day-04/index.py
+++ 2022/day-04/index.py
@@ -4,7 +4,8 @@ puzzle_input = str(utils.get_input(2022, 4))
 
 x = puzzle_input.split('\n')
 
-total = 0
+total_one = 0
+total_two = 0
 
 ranges = [[[int(c) for c in b.split('-') if c != ''] for b in a.split(',')] for a in x]
 
@@ -19,24 +20,12 @@ for r in ranges:
         continue
 
     if (first[0] <= second[0] and first[1] >= second[1]) or (first[0] >= second[0] and first[1] <= second[1]):
-        total += 1
+        total_one += 1
 
-print('Part one:', total)
+    if first[1] >= second[0] and first[0] <= second[1]:
+        total_two += 1
 
 
-total = 0
+print('Part one:', total_one)
 
-for r in ranges:
-    if len(r) != 2:
-        continue
-
-    first = r[0]
-    second = r[1]
-
-    if len(first) != 2 or len(second) != 2:
-        continue
-
-    if (first[1] >= second[0] and first[0] <= second[1]) or (second[1] >= first[0] and second[0] <= first[1]):
-        total += 1
-
-print('Part two:', total)
+print('Part two:', total_two)
